9920b747a2261a887fc5e17fde8f9279953ab9ce
Jamie Wright <54960963+jotadwright@users.noreply.github.com>
2024-01-07 13:00:35 +0100

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import kendalltau
+import matplotlib.pyplot as plt
 
+import seaborn as sns
 
 def rescale(xmin, xmax, ymin, ymax, data):
     """Function rescales from range x to range y"""
@@ -14,6 +16,22 @@ def kendcorr(X, Y):
     corr, p = kendalltau(X, Y)
 
     return corr, p
+
+def plot_icon(x, y, name):
+    # set seaborn style
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots()
+    ax.set_xlim(0.1,0.9)
+    ax.set_ylim(0.1,0.9)
+    ax.set_xticks([0.1,0.3,0.5,0.7,0.9])
+    ax.set_yticks([0.1,0.3,0.5,0.7,0.9])
+    ax.set_title(f'τ = {name}')
+
+    # Custom the color, add shade and bandwidth
+    sns.kdeplot(x=x, y=y, cmap="mako_r", fill=True, cbar=True, cbar_kws={'label': 'Density'})
+    plt.savefig(f'{y.name}.png', format="png")
+    plt.show()
+
 
 # Get predictions from the three models and combine into one dataframe.
 phon = pd.read_csv("data/icon_phon.csv")
@@ -55,6 +73,15 @@ phon_max.to_csv('data/phon_max.csv', index=False)
 sem_max.to_csv('data/sem_max.csv', index=False)
 comb_max.to_csv('data/comb_max.csv', index=False)
 
+# df = df.sample(500, random_state=33)
+highlights = ["shh","hooch","clue","err","era","dwell","sooth","fern","unfulfilled","pain"]
 
+phon_hl = df[['Word','Iconicity','phon_pred']]
+phon_hl = phon_hl.set_index('Word')
+phon_hl = phon_hl.loc[highlights]
+print(phon_hl)
 
+plot_icon(df['Iconicity'], df['phon_pred'], round(phon_tau[0], 2))
+plot_icon(df['Iconicity'], df['sem_pred'], round(sem_tau[0], 2))
+plot_icon(df['Iconicity'], df['comb_pred'], round(comb_tau[0], 2))
 
